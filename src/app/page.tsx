1ed2e96d7bda9e38ee40d13a711aab40fc6493a5
Jahfar Chenakkal <jahfar@Jahfars-MacBook-Pro-2.local>
2023-11-06 15:07:42 +0530

    updated home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <div>
-      <h1>This is home page</h1>
+      <h1>This is my home page</h1>
       <Image src={'/images/kodak.jpeg'} alt='Sample image' width={300} height={300}/>
     </div>
   )
